feef0c1b5153f8057b2e026319d541ac397f8cab
obvTiger <obvtiger@wireway.ch>
2024-02-22 08:37:47 +0000

    inital release

---
+++ assets/journaled.js
@@ -0,0 +1,223 @@
+
+function copyToClipboard(text) {
+    const el = document.createElement('textarea');
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    showToast('Copied to clipboard', 'success');
+}
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    const formTitle = document.getElementById('formTitle').innerText;
+    const name = document.getElementById('name').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const token = document.getElementById('token').value.trim();
+
+    if (!name || !password || !token) {
+        showToast('All fields are required!', 'warning');
+        return;
+    }
+
+    const url = formTitle === 'Login' ? '/login' : '/register';
+    const body = formTitle === 'Login' ? { name, password, token } : { name, password, token, secret };
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        });
+
+        const data = await response;
+        if (response.ok) {
+            data.json().then(jsonData => {
+                document.cookie = `token=${jsonData.session.sessionId}; path=/`;
+            });
+            showToast('Success!', 'success');
+        } else {
+            data.text().then(text => showToast('Error: ' + text, 'danger'));
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        showToast('Error: ' + error, 'danger');
+    }
+}
+
+function switchForm() {
+    const formTitle = document.getElementById('formTitle');
+    const registerFields = document.getElementById('registerFields');
+    const switchFormText = document.getElementById('switchFormText');
+    const form = document.getElementById('loginForm');
+
+    if (formTitle.innerText === "Login") {
+        formTitle.innerText = "Register";
+        registerFields.style.display = "block";
+        switchFormText.innerHTML = "<a class='loginlink' href='#' onclick='switchForm()'>Login</a>";
+        form.querySelector('input[type="submit"]').value = "Register";
+    } else {
+        formTitle.innerText = "Login";
+        registerFields.style.display = "none";
+        switchFormText.innerHTML = "<a class='loginlink' href='#' onclick='switchForm()'>Register</a>";
+        form.querySelector('input[type="submit"]').value = "Login";
+    }
+}
+
+function showToast(message, type = 'success') {
+    const toast = document.createElement('div');
+    toast.style.position = 'fixed';
+    toast.style.top = '20px';
+    toast.style.right = '-300px'; // Start off screen
+    toast.style.backgroundColor = getToastBackgroundColor(type);
+    toast.style.color = 'white';
+    toast.style.padding = '10px';
+    toast.style.borderRadius = '5px';
+    toast.style.zIndex = '1000';
+    toast.style.transition = 'right 0.5s ease-in-out';
+    toast.innerText = message;
+
+    document.body.appendChild(toast);
+
+    // Slide in
+    setTimeout(() => {
+        toast.style.right = '20px';
+    }, 100);
+
+    // Slide out and remove after 4 seconds
+    setTimeout(() => {
+        toast.style.right = '-300px';
+        setTimeout(() => {
+            document.body.removeChild(toast);
+        }, 500); // Wait for slide out to finish
+    }, 4000);
+}
+
+function getToastBackgroundColor(type) {
+    switch (type) {
+        case 'success':
+            return '#2dc937'; // Light green
+        case 'danger':
+            return '#cc3232'; // Light red
+        case 'warning':
+            return '#db7b2b'; // Gold
+        default:
+            return '#D3D3D3'; // Light gray
+    }
+}
+
+function blockInputsAndShowOverlay(block, inactivityTime) {
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach(input => {
+        input.disabled = block;
+    });
+
+    let overlay = document.getElementById('noUserOverlay');
+    if (!overlay) {
+        overlay = document.createElement('div');
+        overlay.id = 'noUserOverlay';
+        overlay.style.position = 'fixed';
+        overlay.style.top = '0';
+        overlay.style.left = '0';
+        overlay.style.width = '100%';
+        overlay.style.height = '100%';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+        overlay.style.zIndex = '1000';
+        overlay.style.display = 'flex';
+        overlay.style.justifyContent = 'center';
+        overlay.style.alignItems = 'center';
+        overlay.style.color = 'white';
+        overlay.style.fontSize = '24px';
+        document.body.appendChild(overlay);
+    }
+
+    if (block) {
+        const loggedOutin = 30000 - inactivityTime
+        const loggedOutInSub = loggedOutin / 1000;
+        overlay.innerText = 'No user detected. Please return to continue.\nYou will be logged out in ' + loggedOutInSub.toFixed(0) + " seconds";
+        overlay.style.display = 'flex';
+        document.getElementById("journalContentTextContainer").style.filter = "blur(8px)";
+    } else {
+        overlay.style.display = 'none';
+        document.getElementById("journalContentTextContainer").style.filter = "";
+    }
+}
+
+async function submitEntry() {
+    const content = document.getElementById('entryContent').value;
+    const sessionId = getCookie('token'); // Assuming the session ID is stored in a cookie named 'token'
+    try {
+        const response = await fetch('/create-entry', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ content, sessionId }),
+        });
+        if (!response.ok) {
+            throw new Error('Failed to create entry');
+        }
+        const result = await response.json();
+        console.log('Entry created successfully', result);
+        fetchEntries()
+        document.getElementById('entryContent').value = "";
+        showToast('Entry has been created', 'success');
+
+
+    } catch (error) {
+        console.error('Error creating entry:', error);
+        // Handle error, e.g., showing an error message
+    }
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+
+async function fetchEntries() {
+    const sessionId = getCookie('token'); // Assuming the session ID is stored in a cookie named 'token'
+    try {
+        const response = await fetch('/entries/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${sessionId}`
+            },
+        });
+        if (!response.ok) {
+            throw new Error('Failed to fetch entries');
+        }
+        const entries = await response.json();
+        console.log('Entries fetched successfully', entries);
+
+        const journalDiv = document.getElementById('journalContentText');
+        let lastDate = null;
+        journalDiv.innerHTML = ''; // Clear existing content
+
+        entries.reverse(); // Reverse the order of entries
+
+        entries.forEach(entry => {
+            const entryDate = new Date(entry.timestamp).toLocaleDateString();
+            if (lastDate !== entryDate) {
+                const dateElement = document.createElement('div');
+                dateElement.innerHTML = `<strong>${entryDate}</strong>`;
+                journalDiv.appendChild(dateElement);
+                lastDate = entryDate;
+            }
+            const time = new Date(entry.timestamp).toLocaleTimeString();
+            const entryElement = document.createElement('div');
+            entryElement.textContent = `${time}: ${entry.content}`;
+            journalDiv.appendChild(entryElement);
+        });
+        // Handle displaying the entries here, e.g., updating the DOM
+    } catch (error) {
+        console.error('Error fetching entries:', error);
+        // Handle error, e.g., showing an error message
+    }
+}
---
+++ server.js
@@ -0,0 +1,238 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const speakeasy = require('speakeasy');
+const QRCode = require('qrcode');
+var cors = require('cors')
+const bodyParser = require('body-parser');
+
+const app = express();
+const prisma = new PrismaClient();
+const port = 3017;
+
+app.use(cors())
+app.use(bodyParser.json());
+
+// Generate TOTP QR Code
+app.get('/generate-totp/:name', async (req, res) => {
+  const { name } = req.params;
+  const secret = speakeasy.generateSecret({ length: 20, name: "Journaled: " + name });
+  QRCode.toDataURL(secret.otpauth_url, (err, data_url) => {
+    if (err) {
+      return res.status(500).send('Error generating QR code');
+    }
+    res.json({ qrCode: data_url, secret: secret.base32 });
+  });
+});
+
+const bcrypt = require('bcrypt');
+
+// Register route
+app.post('/register', async (req, res) => {
+  const { name, password, token, secret } = req.body;
+  const verified = speakeasy.totp.verify({
+    secret: secret,
+    encoding: 'base32',
+    token: token,
+  });
+
+  if (!verified) {
+    return res.status(400).send('Invalid token');
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const encryptedSecret = encrypt(secret);
+    const user = await prisma.user.create({
+      data: {
+        name,
+        password: hashedPassword,
+        TOTP: encryptedSecret,
+      },
+    });
+    const sessionId = require('crypto').randomBytes(64).toString('hex');
+    const session = await prisma.session.create({
+      data: {
+        userId: user.id,
+        sessionId: sessionId,
+      },
+    });
+    res.json({ user, session });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Error creating user');
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { name, password, token } = req.body;
+  const user = await prisma.user.findUnique({
+    where: {
+      name,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).send('User not found');
+  }
+
+  const decryptedSecret = decrypt(user.TOTP);
+
+  const verified = speakeasy.totp.verify({
+    secret: decryptedSecret,
+    encoding: 'base32',
+    token: token,
+  });
+
+  if (!verified) {
+    return res.status(400).send('Invalid token');
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res.status(401).send('Invalid password');
+  }
+
+
+  const sessionId = require('crypto').randomBytes(64).toString('hex');
+  const session = await prisma.session.create({
+    data: {
+      userId: user.id,
+      sessionId: sessionId,
+    },
+  });
+
+  res.json({ message: 'Logged in successfully', session });
+});
+
+const crypto = require('crypto');
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+const algorithm = 'aes-256-ctr';
+
+// Function to encrypt the content
+function encrypt(text) {
+  const key = crypto.scryptSync(ENCRYPTION_KEY, 'salt', 32);
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return iv.toString('hex') + ':' + encrypted;
+}
+
+
+function decrypt(text) {
+  let textParts = text.split(':');
+  let iv = Buffer.from(textParts.shift(), 'hex');
+  let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  const key = crypto.scryptSync(ENCRYPTION_KEY, 'salt', 32);
+  let decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+
+app.get('/entries/', async (req, res) => {
+  const sessionId = req.headers.authorization.split(' ')[1];
+  try {
+    const session = await prisma.session.findFirst({
+      where: {
+        sessionId,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!session) {
+      return res.status(404).send('Session not found');
+    }
+
+    const entries = await prisma.entries.findMany({
+      where: {
+        userId: session.userId,
+      },
+    });
+
+    const decryptedEntries = entries.map(entry => {
+      return {
+        ...entry,
+        content: decrypt(entry.content),
+      };
+    });
+
+    res.json(decryptedEntries);
+  } catch (error) {
+    res.status(500).send('Error retrieving entries');
+  }
+});
+
+app.post('/create-entry', async (req, res) => {
+  const { content, sessionId } = req.body;
+  if (!sessionId || !content) {
+    return res.status(400).send('Session ID and content are required');
+  }
+  try {
+    const session = await prisma.session.findFirst({
+      where: {
+        sessionId: sessionId,
+      },
+    });
+
+    if (!session) {
+      return res.status(404).send('Session not found');
+    }
+
+    const encryptedContent = encrypt(content);
+
+    const entry = await prisma.entries.create({
+      data: {
+        userId: session.userId,
+        content: encryptedContent,
+      },
+    });
+
+    res.json({ message: 'Entry created successfully', entry: { ...entry, content } });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error creating entry');
+  }
+});
+
+app.post('/verify-session', async (req, res) => {
+  const { sessionId } = req.body;
+  if (!sessionId) {
+    return res.status(400).send('Session ID is required');
+  }
+  try {
+    const session = await prisma.session.findFirst({
+      where: {
+        sessionId: sessionId,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!session) {
+      return res.status(404).send('Session not found');
+    }
+    res.status(200).send('Token is valid');
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error verifying token');
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/ui', (req, res) => {
+  res.sendFile(__dirname + '/ui.html');
+});
+
+app.use('/humanModel', express.static('humanModel'));
+app.use('/assets', express.static('assets'));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
